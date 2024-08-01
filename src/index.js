616271a2b7d09f7a6bb5d899530c80cc7fd84d6d
Olavo15 <olavoregis169@gmail.com>
2024-08-01 16:15:11 -0300

    Main prompt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import prompt from 'prompt'
-async function main(){}
+import prompt from 'prompt';
+import mainPrompt from './prompts/prompt-main.js';
+
+async function main(){
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log("escolheu o qrcode")
+        if(choose.select == 2) console.log("escolheu o password")
+
+    });
+}
 
 main();
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,9 +1,11 @@
 const mainPrompt = [
     {
         name:"select",
-        description: "Escolha a ferramenta (1 - QRCODE ou 2-PASSWORD",
+        description: "Escolha a ferramenta (1 - QRCODE ou 2-PASSWORD)",
         pattern:/^[1-2]+$/,
         message: "Escolha apenas 1 e 2",
         require: true,
     },
 ]
+
+export default mainPrompt;
